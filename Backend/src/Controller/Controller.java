26964cfce72614d447fc79edc327fde18074440f
Azurest <daniel1511@live.dk>
2014-03-14 10:38:56 +0100

    added descriptions

--- Backend/src/Controller/Controller.java
+++ Backend/src/Controller/Controller.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/**
+ * Defines the overall functions for Security controller and facade controller
+ */
 package Controller;
 
 /**
