59b7f3df63e6e3d438617f4e6f0382f436309982
Emil Juul Jacobsen <=>
2014-03-14 12:03:30 +0100

    deleting and renaming

---
+++ Netbeans Project/README.md
@@ -0,0 +1,3 @@
+sas
+===
+Taking you higher since 1999.
---
+++ Netbeans Project/src/java/Controller/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Defines the overall functions for Security controller and facade controller
+ */
+package Controller;
+
+/**
+ *
+ * @author dst
+ */
+public interface Controller {
+    
+}
