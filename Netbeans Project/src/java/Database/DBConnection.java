e1d4031d13ef6a3e8ade07a3e9d4442c4ef93d8a
Martin Marcher <marcher1210@gmail.com>
2014-03-20 20:13:43 +0100

    First (insecure) implementation of Database connection. Probably doesn't really work

--- Netbeans Project/src/java/Database/DBConnection.java
+++ Netbeans Project/src/java/Database/DBConnection.java
@@ -3,35 +3,119 @@
  * and open the template in the editor.
  */
 package Database;
+
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import java.io.IOException;
 import java.sql.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  *
  * @author marcher89
  */
 public class DBConnection {
-    private static Map<String, String[]> userList = new HashMap<>();
+
+    private static Map<String, String[]> roles = new HashMap<>();
     private static String url;
     private static int port;
     private static String database;
     
-    public static void main(String[] args) {
+    @Resource(name="jdbc/login")
+    private static DataSource loginDatasource;
+    @Resource(name="jdbc/user")
+    private static DataSource userDatasource;
+    @Resource(name="jdbc/admin")
+    private static DataSource adminDatasource;
+    
+    
+    private static Connection loginConnection;
+    private static Connection  userConnection;
+    private static Connection adminConnection;
+
+    /* "Constructor" */
+    static {
         loadXML();
+        /*try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            //TODO: Error handling
+            ex.printStackTrace();
+        }*/
+    }
+
+    public static void main(String[] args) {
         System.out.println(url);
         System.out.println(port);
         System.out.println(database);
+        System.out.println(validUserLogin("usder", "password"));
+        System.out.println(validUserLogin("user", "pasdsword"));
+        System.out.println(validUserLogin("user", "password"));
     }
     
+    public static boolean validUserLogin(String username, String cleartextPassword){
+        try {
+            Connection conn = getLoginConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery("SELECT COUNT(*) AS count FROM User WHERE username = '"+username+"' AND password = MD5('"+cleartextPassword+"');");
+            stmt.getResultSet().next();
+            int count = (int) (long)stmt.getResultSet().getObject(1);
+            return count > 0;
+        } catch (SQLException ex) {
+            //Unable to create prepared statement
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private static Connection getLoginConnection() {
+        try {
+            Context ctx = new InitialContext();
+            return ((DataSource)ctx.lookup("java:comp/env/jdbc/login")).getConnection();
+            //return loginDatasource.getConnection();
+        } catch (Exception ex) {
+            //TODO: Error handling
+            ex.printStackTrace();
+        }
+        if(loginConnection == null) loginConnection = getConnection("login");
+        return loginConnection;
+    }
+
+    private static Connection getUserConnection() {
+        if(userConnection == null) userConnection = getConnection("user");
+        return userConnection;
+    }
+
+    private static Connection getAdminConnection() {
+        if(adminConnection == null) adminConnection = getConnection("admin");
+        return adminConnection;
+    }
+    
+    private static Connection getConnection(String role) {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + database, roles.get(role)[0], roles.get(role)[1]);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            //TODO: Error handling
+        }
+        return con;
+    }
+
     private static void loadXML() {
         try {
             DOMParser parser = new DOMParser();
-            parser.parse("src/database/SQLConnection.conf.xml");
+            parser.parse("src/java/Database/SQLConnection.conf.xml");
             Document doc = parser.getDocument();
-            
+
             //Get host connection string
             {
                 Node host = doc.getElementsByTagName("host").item(0);
@@ -49,9 +133,9 @@ public class DBConnection {
                 Node conn = userNode.getChildNodes().item(1);
                 attr = conn.getAttributes();
                 String user = attr.getNamedItem("user").getNodeValue();
-                String password = attr. getNamedItem("password").getNodeValue();
+                String password = attr.getNamedItem("password").getNodeValue();
                 String[] strs = {user, password};
-                userList.put(role, strs);
+                roles.put(role, strs);
             }
         } catch (SAXException ex) {
             ex.printStackTrace();
